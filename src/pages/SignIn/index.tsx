21460b1e505ee745236674e2659f0db53bcfc55b
pmapelli <pmapelli@hotmail.com>
2020-07-13 08:22:53 -0300

    feat: Isolating components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -10,9 +12,14 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="senha"
+        />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot"> Esqueci minha senha</a>
       </form>
