07770d6e7137fc77ee497e7ca64426b09b157132
pmapelli <pmapelli@hotmail.com>
2020-07-13 08:32:01 -0300

    feat: created register page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
